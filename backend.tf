6289f5ef670fb74d541cd375016cf01cf16e0514
Rkeram <65199967+Rkeram@users.noreply.github.com>
2023-04-07 18:08:03 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
-    key = "jenkins-server/terraform.tfstate"
+    bucket         = "rks1932"
+    key            = "terraform.tfstate"
+    region         = "ap-northeast-1"
   }
 }
