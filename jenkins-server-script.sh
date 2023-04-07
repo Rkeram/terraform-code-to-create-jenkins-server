e9504888e5590eba81df1a96cabbec0cf45ef796
Rkeram <65199967+Rkeram@users.noreply.github.com>
2023-04-07 18:20:19 +0530

    Update jenkins-server-script.sh

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -1,36 +1,27 @@
+#!/bin/bash
+
+# Update the package list and install OpenJDK 11
 sudo apt update -y
-sudo apt install openjdk-11-jre -y 
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y 
+sudo apt install openjdk-11-jre -y
+
+# Add the Jenkins repository key and package source
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update the package list again and install Jenkins
+sudo apt-get update -y
 sudo apt-get install jenkins -y
 
-
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl
-sudo apt-get install -y apt-transport-https
+# Add the Kubernetes repository key and package source
+sudo apt-get install -y ca-certificates curl apt-transport-https
 sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update
-
+# Update the package list again and install kubectl
+sudo apt-get update -y
 sudo apt-get install -y kubectl
-sudo add-apt-repository ppa:git-core/ppa 
 
-
-sudo apt-get update && sudo apt-get -y install git 
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
---fingerprint
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt-get install terraform
+# Add the Git PPA repository and install Git
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update -y
+sudo apt-get install -y git
